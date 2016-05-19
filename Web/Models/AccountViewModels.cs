53b71c27bb8034a7d027eb903becb4edfa1a4c4f
PHANAPHONSUWANMANEE <phanaphon_s@cmu.ac.th>
2016-05-20 00:05:00 +0700

    register model

--- Web/Models/AccountViewModels.cs
+++ Web/Models/AccountViewModels.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace Web.Models
@@ -79,6 +80,43 @@ namespace Web.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Phone is required")]
+        [Phone]
+        [Display(Name = "PhoneNumber")]
+        public string PhoneNumber { get; set; }
+
+
+        [Required(ErrorMessage = "First Name is required")]
+        [DisplayName("First Name")]
+        [StringLength(160)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        [DisplayName("Last Name")]
+        [StringLength(160)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        [StringLength(70)]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "City is required")]
+        [StringLength(40)]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Province is required")]
+        [StringLength(40)]
+        public string Province { get; set; }
+
+        [Required(ErrorMessage = "Postal Code is required")]
+        [DisplayName("Postal Code")]
+        [StringLength(10)]
+        public string PostalCode { get; set; }
+
+        [Required(ErrorMessage = "Country is required")]
+        [StringLength(40)]
+        public string Country { get; set; }
     }
 
     public class ResetPasswordViewModel
