49a31c4d41f4618396606ac0494d59523007e890
devilsjackiez <jackrin_pat@cmu.ac.th>
2016-05-20 13:29:36 +0700

    ProductViewModel

---
+++ Model/Dto/ProductViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Dto
+{
+    public class ProductViewModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Price")]
+        public double Price { get; set; }
+
+        [Required]
+        [Display(Name = "Amount")]
+        public int Amount { get; set; }
+    }
+}
